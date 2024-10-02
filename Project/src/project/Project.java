956f8ba7096c167cae6dd429aa6a21f5ca13a506
Dell <Dell@DESKTOP-32NF92G>
2024-10-02 10:50:05 +0530

    makuwa

--- Project/src/project/Project.java
+++ Project/src/project/Project.java
@@ -14,7 +14,7 @@ public class Project {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("el");
+//       Comment
     }
     
 }
